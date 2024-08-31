2033517c553e17fe379c97638d3f49c07be22f7a
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-08-31 23:46:24 +0530

    hook up the Interpreter class to the interpreter

--- src/lox/Interpreter.java
+++ src/lox/Interpreter.java
@@ -1,6 +1,15 @@
 package lox;
 
 class Interpreter implements Expr.Visitor<Object> {
+    void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
     @Override
     public Object visitConditionalExpr(Expr.Conditional expr) {
         Object result = evaluate(expr.condition);
@@ -63,6 +72,20 @@ class Interpreter implements Expr.Visitor<Object> {
         if (a == null) return false;
 
         return a.equals(b);
+    }
+
+    private String stringify(Object object) {
+        if (object == null) return "nil";
+
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+
+        return object.toString();
     }
 
     private Object evaluate(Expr expr) {
